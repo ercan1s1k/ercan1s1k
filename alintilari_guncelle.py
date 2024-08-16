e6c53af4b8d6091109149be235ef281ee9d781d1
ercan1s1k <113260584+ercan1s1k@users.noreply.github.com>
2024-08-16 22:47:51 +0300

    Update alintilari_guncelle.py

--- alintilari_guncelle.py
+++ alintilari_guncelle.py
@@ -1,14 +1,3 @@
-# Alıntılar dosyasının adı
-quotes_file = 'alintilar.txt'
-
-# GitHub profil README dosyasının adı
-readme_file = 'README.md'
-
-def read_quotes(file):
-    with open(file, 'r', encoding='utf-8') as f:
-        quotes = f.readlines()
-    return quotes
-
 def update_readme(quotes, readme):
     with open(readme, 'r', encoding='utf-8') as f:
         content = f.readlines()
@@ -26,6 +15,13 @@ def update_readme(quotes, readme):
     with open(readme, 'w', encoding='utf-8') as f:
         f.writelines(new_content)
 
+    # Debug print statements
+    print("Alıntılar eklendi:")
+    print(new_content[start_index:end_index])
+
 if __name__ == '__main__':
+    quotes_file = 'alintilar.txt'
+    readme_file = 'README.md'
     quotes = read_quotes(quotes_file)
+    print("Okunan alıntılar:", quotes)  # Hata ayıklama mesajı
     update_readme(quotes, readme_file)
